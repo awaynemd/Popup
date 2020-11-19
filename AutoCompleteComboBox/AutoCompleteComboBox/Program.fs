e8bbd518596719dab51463469269d35f67d716f8
Tyson Williams <tyson.williams@blocherconsulting.com>
2020-11-19 15:41:34 -0600

    got ComboBox filtering to work and simplified code

--- AutoCompleteComboBox/AutoCompleteComboBox/Program.fs
+++ AutoCompleteComboBox/AutoCompleteComboBox/Program.fs
@@ -1,6 +1,7 @@
 ﻿namespace AutoCompleteComboBox
 
 open System
+open Elmish
 open Elmish.WPF
 open System.Windows
 
@@ -8,92 +9,52 @@ open System.Windows
 module App =
 
     type Model =
-       { Visibility: System.Windows.Visibility
-         IsOpen: bool
-         AutoSuggestionList: string list
-         Suggestions: string list option
-         Text: string option
+       { IsOpen: bool
+         Suggestions: string list
+         Text: string
          SelectedItem: string option
        }
 
-    let init () = {
-      Visibility = Visibility.Collapsed  
-      IsOpen = true
-      AutoSuggestionList = [ "Alabama"; "Alaska"; "Arizona"; "Arkansas"; "California"; "Colorado"; "Connecticut"; 
-                             "Delaware"; "Florida"; "Georgia"; "Hawaii"; "Idaho"; "Illinois"; "Indiana"; "Iowa" ]
-      Text = Some "Hi"
-      Suggestions = None
-      SelectedItem = None
+    let init () =
+      { IsOpen = false
+        Suggestions = [ "Alabama"; "Alaska"; "Arizona"; "Arkansas"; "California"; "Colorado"; "Connecticut"; 
+                        "Delaware"; "Florida"; "Georgia"; "Hawaii"; "Idaho"; "Illinois"; "Indiana"; "Iowa" ]
+        Text = "A"
+        SelectedItem = None
       }
 
     type Msg =
       | SetOpen of bool
-      | SetText of string option
+      | SetText of string
       | SetSelectedItem of string option
-      | AutoListSelectionChanged
-      | AutoTextBoxTextChanged
-      | MakeVisible
-      | Collapse
-      | NoOp
 
     let CloseAutoSuggestionBox m =
-        { m with Visibility = Visibility.Collapsed; IsOpen = false }  
+        { m with IsOpen = false }
 
     let OpenAutoSuggestionBox m  =
-         { m with Visibility = Visibility.Visible; IsOpen = true } 
+         { m with IsOpen = true }
 
-    let Suggest m =
-        match m.Text with
-        | Some t -> 
-            let SuggestionList =
-                m.AutoSuggestionList |> List.filter (fun s -> s.ToLower().Contains(t.ToLower())) |> Some
-            {m with Suggestions = SuggestionList }                                 //|> printfn "list is %A"
-        | None -> m
-
-    let handleAutoListSelectionChanged m =
-        match m.SelectedItem with
-        | Some i -> m |> CloseAutoSuggestionBox |> (fun m -> {m with Text = Some i})
-        | None -> m |> CloseAutoSuggestionBox |> (fun m -> {m with Text = None})
-        
-
-    let handleAutoTextBoxTextChanged m =
-        let notNullOrEmpty = not << System.String.IsNullOrEmpty
-
-        match m.Text with
-        | None  -> CloseAutoSuggestionBox m          
-        | _ -> m |> OpenAutoSuggestionBox |> Suggest 
-      
+    let filteredSuggestions m =
+        m.Suggestions |> List.filter (fun s -> s.ToLower().Contains(m.Text.ToLower()))
 
     let update msg m  =
         match msg with
         | SetOpen o -> { m with IsOpen = o }
-        | SetText s  -> { m with Text = s }
-        | SetSelectedItem i -> {m with SelectedItem = i }
-        | AutoListSelectionChanged -> m |> handleAutoListSelectionChanged
-        | AutoTextBoxTextChanged -> m |> handleAutoTextBoxTextChanged
-        | MakeVisible
-        | Collapse
-        | NoOp -> m
+        | SetText s -> { m with Text = s; IsOpen = true }
+        | SetSelectedItem i -> { m with SelectedItem = i; IsOpen = i.IsNone; Text = if i.IsSome then "" else m.Text }
 
-    
-
-    let bindings()  = [     
-       "Suggestions" |> Binding.oneWay ( fun m -> 
-                                                    match m.Suggestions with
-                                                    | Some s -> s
-                                                    | None -> []
-                                        )
-       "Visibility" |> Binding.oneWay ( fun m -> m.Visibility)
-       "IsOpen"  |> Binding.twoWay ( (fun m -> m.IsOpen), SetOpen)
-       "Text"  |> Binding.twoWayOpt((fun m -> m.Text),  SetText)
-       "SelectedSuggestion"     |> Binding.twoWayOpt((fun m -> m.SelectedItem), SetSelectedItem)
-       "AutoTextBoxTextChanged" |> Binding.cmd AutoTextBoxTextChanged
-       "AutoListSelectionChanged" |> Binding.cmd AutoListSelectionChanged
+    let bindings () = [
+       "Suggestions" |> Binding.oneWay filteredSuggestions
+       "IsOpen" |> Binding.twoWay ( (fun m -> m.IsOpen), SetOpen)
+       "Text" |> Binding.twoWay((fun m -> m.Text), SetText)
+       "SelectedSuggestion" |> Binding.twoWayOpt((fun m -> m.SelectedItem), SetSelectedItem)
     ]
 
-    [<EntryPoint; STAThread>]
-    let main argv =
+    let vm = ViewModel.designInstance (init ()) (bindings ())
+
+    let main window =
       Program.mkSimpleWpf init update bindings
+      |> Program.withConsoleTrace
       |> Program.runWindowWithConfig
          { ElmConfig.Default with LogConsole = true }      
-         (AutoCompleteComboBox.Views.MainWindow())
+         window
